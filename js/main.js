7d0a22f9365769f63984649e1c6061030b856d98
nader1978 <51798004+nader1978@users.noreply.github.com>
2021-09-29 10:10:03 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,8 +63,8 @@
         var typed = new Typed('.hero .hero-text h2',
             {
             strings: typed_strings.split(','),
-            typeSpeed: 250,
-            backSpeed: 250,
+            typeSpeed: 150,
+            backSpeed: 150,
             smartBackspace: false,
             loop: true
         });
